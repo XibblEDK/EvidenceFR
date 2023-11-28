ee2a3f0909dea7dd825cf0da0bdb5a629f28f40c
Adam Nielsen <adam.nielsen@outlook.dk>
2023-11-28 20:20:59 +0100

    Created a basic LSPDFR Plugin structure.

--- EvidenceFR/Main.cs
+++ EvidenceFR/Main.cs
@@ -1,7 +1,31 @@
-﻿namespace EvidenceFR
-{
-    public class Main
-    {
+﻿using LSPD_First_Response.Mod.API;
+using Rage;
+using System;
+using System.IO;
+using System.Reflection;
 
+namespace EvidenceFR
+{
+    public class Main : Plugin
+    {
+        private bool isRunning;
+
+        public override void Initialize() => Functions.OnOnDutyStateChanged += new Functions.OnDutyStateChangedEventHandler(this.Functions_OnOnDutyStateChanged);
+
+        public override void Finally()
+        {
+            if (!this.isRunning)
+                return;
+            EvidenceFR.Instance.Stop();
+            this.isRunning = false;
+        }
+
+        private void Functions_OnOnDutyStateChanged(bool onDuty)
+        {
+            if (!onDuty)
+                return;
+            EvidenceFR.Instance.Start();
+            this.isRunning = true;
+        }
     }
 }
---
+++ EvidenceFR/Utils/Logging.cs
@@ -0,0 +1,45 @@
+﻿using Rage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidenceFR.Utils
+{
+    public class Logging
+    {
+        internal enum LogLevel
+        {
+            Debug,
+            Info,
+            Warning,
+            Error
+        }
+
+        internal static void Log(LogLevel logLevel, string logMessage)
+        {
+            string logLevelImportance = string.Empty;
+            switch (logLevel)
+            {
+                case LogLevel.Debug:
+                    logLevelImportance = "[DEBUG]: ";
+                    break;
+                case LogLevel.Info:
+                    logLevelImportance = "[INFO]: ";
+                    break;
+                case LogLevel.Warning:
+                    logLevelImportance = "[WARNING]: ";
+                    break;
+                case LogLevel.Error:
+                    logLevelImportance = "[ERROR]: ";
+                    break;
+            }
+
+            Game.LogTrivial(logLevelImportance + logMessage);
+        }
+
+    }
+}
