069c01e927b6d7f62db58ea7729df0eb09d6dc01
Veteranfighter <lenge099@gmail.com>
2023-11-29 21:56:57 +0100

    Initial commit

---
+++ EvidenceFR/Functions/Object/EvidenceCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidenceFR.Functions.Object
+{
+    public class EvidenceCase
+    {
+        private List<EvidenceEntity> evidenceEntities = new List<EvidenceEntity>();
+
+    }
+}
