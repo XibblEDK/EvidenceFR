debc45a6f78703529fdfc7ad714ee5028efaafa0
Adam Nielsen <adam.nielsen@outlook.dk>
2023-11-29 20:59:33 +0100

    Created some prop enums.

---
+++ EvidenceFR/Mod/Props/EDrugProps.cs
@@ -0,0 +1,20 @@
+﻿using EvidenceFR.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidenceFR.Mod.Props
+{
+    public class EDrugProps : EProp
+    {
+        public static EDrugProps DrugTube => new("ng_proc_drug01a002", "Drug Tube");
+        public static EDrugProps DrugBottle => new("prop_drug_bottle", "Drug Bottle");
+        public static EDrugProps NeedleAndDrugTubes => new("v_61_bed1_mesh_drugstuff", "Needle And Drug Tubes");
+        public static EDrugProps WeedBottle => new("prop_weed_bottle", "Weed Bottle");
+
+        public EDrugProps(string propModelName, string name)
+            : base(propModelName, name) { }
+    }
+}
